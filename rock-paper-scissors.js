790c304c688b4cd4c8f301cf01c8e3c77247c4e1
Hamza Mujinovic <mujinovichamza@gmail.com>
2023-08-03 11:17:54 +0200

    Update the playRound function
    The function now does the same, but with less code.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -6,36 +6,21 @@ function getComputerChoice(elements) {
 }
 
 
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection === 'rock' && computerSelection === 'Scissors') {
-    const rockwin = ('You win! Rock beats Scissors!');
-    return rockwin;
-    } else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'Paper') {
-        const rocklose = ('You lose! Paper beats Rock!');
-        return rocklose;
-    } else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'Rock') {
-        const rockdraw = ('Draw!')
-        return rockdraw;
-    } else if ( playerSelection.toLowerCase() === 'paper' && computerSelection === 'Paper') {
-        const paperdraw = ('Draw!');
-        return paperdraw;
-    } else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'Rock') {
-        const paperwin = ('You win! Paper beats Rock!');
-        return paperwin;
-    } else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'Scissors') {
-        const paperlose = ('You lose! Scissors beat Paper');
-        return paperlose;
-    } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'Scissors') {
-        const scissorsdraw = ('Draw!');
-        return scissorsdraw;
-    } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'Rock') {
-        const scissorslose = ('You lose! Rock beats Scissors!');
-        return scissorslose;
-    } else (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'Paper');
-        const scissorswin = ('You win! Scissors beat Paper');
-        return scissorswin;
-}
+function playRound(playerChoice, computerChoice) {
+    if (playerChoice === computerChoice) {
+      return 'It\'s a tie!';
+    } else if (
+      (playerChoice === 'rock' && computerChoice === 'scissors') ||
+      (playerChoice === 'paper' && computerChoice === 'rock') ||
+      (playerChoice === 'scissors' && computerChoice === 'paper')
+    ) {
+      return `You win! ${playerChoice} beats ${computerChoice}.`;
+    } else {
+      return `You lose! ${computerChoice} beats ${playerChoice}.`;
+    }
+  }
 
+  
 
 function game() {
     let playerScore = 0;
