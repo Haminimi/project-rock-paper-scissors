d86a151be0da92ccc8840ab20674a511db424ede
Hamza Mujinovic <mujinovichamza@gmail.com>
2023-08-03 17:14:49 +0200

    Add the event listeners to the buttons

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -5,6 +5,10 @@ const resultsDiv = document.getElementById('results');
 const scorePara = document.getElementById('scorePara');
 const winner = document.getElementById('winner');
 
+rockButton.addEventListener('click', () => playRound('Rock'));
+paperButton.addEventListener('click', () => playRound('Paper'));
+scissorsButton.addEventListener('click', () => playRound('Scissors'));
+
 const elements = ['Rock', 'Paper', 'Scissors'];
 function getComputerChoice(elements) {
     const random = Math.floor(Math.random() * elements.length);
